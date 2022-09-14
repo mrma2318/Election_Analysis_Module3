367d3ae4c8f9fc8645068a62627ccbe4fbae4739
mrma2318 <111613834+mrma2318@users.noreply.github.com>
2022-09-13 19:26:59 -0500

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,40 @@
+# print("Hello World")
+
+counties = ["Arapahoe", "Denver", "Jefferson"]
+county_tuple = ("Arapahoe", "Denver", "Jefferson")
+
+#create a dict
+counties_dict = {}
+counties_dict["Arapahoe"]=422829
+counties_dict["Denver"]=463353
+counties_dict["Jefferson"]=432438
+
+#list of dictionaries
+voting_data =[]
+voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+voting_data.append({"county":"Denver", "registered_voters": 463353})
+voting_data.append({"county":"Jefferson", "registered_voters": 432438})
+
+# # How many votes did you get?
+# my_votes = int(input("How many votes did you get in the election? "))
+# #  Total votes in the election
+# total_votes = int(input("What is the total votes in the election? "))
+# # Calculate the percentage of votes you received.
+# percentage_votes = (my_votes / total_votes) * 100
+# print("I received " + str(percentage_votes)+"% of the total votes.")
+
+
+#refactoring the code to us f strings
+my_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+#multiple f strings
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+print(message_to_candidate)
